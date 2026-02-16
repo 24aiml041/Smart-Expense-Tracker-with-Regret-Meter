5d082511dbd9736fb00685278adef32399f60178
24aiml041 <24aiml041@charusat.edu.in>
2026-02-16 21:02:13 +0530

    Add files via upload
    added header file

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Smart Expenses Tracker</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+<div class="container mt-4">
+<h2 class="text-center mb-4">💰 Smart Expenses Tracker</h2>
